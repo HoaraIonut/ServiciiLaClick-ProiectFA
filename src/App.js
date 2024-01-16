4e36136f9cc83b33a7e5e7554af700f1c8c9de89
Hoară Ionuț <87201884+HoaraIonut@users.noreply.github.com>
2024-01-17 00:39:05 +0200

    Deployed website

--- src/App.js
+++ src/App.js
@@ -22,11 +22,11 @@ const router = createBrowserRouter(
       <Route>
         <Route element={<NavBar />}>
           <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/produs" element={<ProductPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+          <Route path="produs" element={<ProductPage />} />
           <Route element={<ProtectedRoute />}>
-            <Route path="/myaccount" element={<MyAccountPage />} />
+            <Route path="myaccount" element={<MyAccountPage />} />
           </Route>
           <Route path="*" element={<PageNotFound />} />
         </Route>
